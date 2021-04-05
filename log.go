4603ee92eb0be09f98fd95e975a29495840d078b
Edney O. S. Filho <edney.osf@gmail.com>
2021-04-05 09:02:06 -0300

    First commit

---
+++ log.go
@@ -0,0 +1,88 @@
+package gloged
+
+//
+// by Edney O. S. Filho
+//
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+var DebugMode = false
+var WriteFile = true
+var Path = "gloged"
+
+const filePermission = 0755
+const dirPermission = 0755
+const resetColor = string("\033[0m")
+const greenColor = string("\033[32m")
+const redColor = string("\033[31m")
+const yellowColor = string("\033[33m")
+
+func I(text string) {
+	time := getTime()
+	line := time + " • " + text
+
+	writeLog(time, line)
+	fmt.Println(greenColor, line, resetColor)
+}
+
+func D(method string, key string, value string) {
+	if DebugMode {
+		time := getTime()
+		line := time + " ▸ (" + method + ") " + key + " -> |" + value + "|"
+
+		writeLog(time, line)
+		fmt.Println(resetColor, line, resetColor)
+	}
+}
+
+func W(text string) {
+	time := getTime()
+	line := time + " ! " + text
+
+	writeLog(time, line)
+	fmt.Println(yellowColor, line, resetColor)
+}
+
+func E(text string) {
+	time := getTime()
+	line := time + " × " + text
+
+	writeLog(time, line)
+	fmt.Println(redColor, line, resetColor)
+}
+
+func getFileName() string {
+	date := time.Now().Format("02-01-2006")
+	return "log-" + date + ".ed"
+}
+
+func getTime() string {
+	return time.Now().Format("15:04:05")
+}
+
+func writeLog(time string, text string) {
+	if WriteFile {
+		line := time + " " + text + "\n"
+		fileName := getFileName()
+
+		if _, err := os.Stat(Path); os.IsNotExist(err) {
+			os.Mkdir(Path, 0755)
+		}
+
+		f, err := os.OpenFile(Path+"/"+fileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, filePermission)
+
+		if err != nil {
+			panic(err)
+		}
+
+		defer f.Close()
+
+		if _, err = f.WriteString(line); err != nil {
+			panic(err)
+		}
+	}
+}
